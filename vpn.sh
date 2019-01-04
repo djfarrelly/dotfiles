d91619d10043012eefdad6c76d1a0d035dfc577a
Dan Farrelly <daniel.j.farrelly@gmail.com>
2019-01-04 20:05:05 +0000

    Update vpn scripts

---
+++ vpn.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CMD="sudo openvpn --config $HOME/buffer.ovpn"
+# TODO - Maybe we can run w/ --daemon
+
+$CMD
+CODE=$?
+
+# User initiated shutdown
+if [ $CODE -eq 0 ]; then
+  exit 0
+fi
+
+echo "Exit code = $CODE"
+
+printf "
+If you've just got the TUN/TAP error, do the following to fix:
+
+  1. Open crosh from Chrome by pressing Control+Alt+T
+  2. Run: vmc start termina
+  3. Run: lxc config device add penguin tun unix-char path=/dev/net/tun
+  4. Run this script again
+"
